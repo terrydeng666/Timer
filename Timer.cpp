45f6b6b32b539176d92139f21f13e80a7163ad81
donpeyem <77134209+terrydeng666@users.noreply.github.com>
2021-01-20 13:59:23 +0800

    Update Timer.cpp

--- Timer.cpp
+++ Timer.cpp
@@ -2,16 +2,25 @@
 #include <ctime>
 #include <unistd.h>
 
-using namespace std;
+using namespace std; //之後所有的cout 和 cin都是用std
 
 class Timer {
     public:
+    //constructor
+    Timer() {
+        start_ts=0;
+    }
+    Timer(time_t s) {
+        SetStart(s);
+    }
+
     void SetStart(time_t ts) {
         start_ts = ts;
     }
     time_t GetStart() {
         return start_ts;
     }
+    //現在時間減掉初始時間
     int GetElapsedTime() {
         return time(NULL) - GetStart();
     }
@@ -22,9 +31,9 @@ class Timer {
 int main () {
     Timer tmr;
     time_t ts;
+    //初始化
     ts = time(NULL);
     tmr.SetStart(ts);
-    sleep(3);
 
     cout << "Elapsed Time:" << tmr.GetElapsedTime() << endl; 
     return 0;
